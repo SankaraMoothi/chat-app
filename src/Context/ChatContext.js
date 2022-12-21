8f3eb134985bf081ac41ec60999a2e81150c476f
SankaraMoothi <sankaramoorthi99@gmail.com>
2022-12-21 13:25:57 +0530

    FinalCommit

--- src/Context/ChatContext.js
+++ src/Context/ChatContext.js
@@ -1,12 +1,5 @@
-import { onAuthStateChanged } from "firebase/auth";
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useState,
-} from "react";
-import { auth } from "../firebase";
+import { createContext, useContext, useReducer } from "react";
+
 import { AuthContext } from "./AuthContext";
 
 export const ChatContext = createContext();
